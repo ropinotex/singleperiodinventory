01c4568bded4f2b6888e3d0c9b27c4f4339766b8
roberto <roberto.pinto@unibg.it>
2019-09-25 13:44:25 +0200

    new file:   SPinventory.py 	new file:   requirements.txt 	new file:   runtime.txt

---
+++ SPinventory.py
@@ -0,0 +1,10 @@
+# ==============================================================================
+# description     :Single period inventory analysis toolbox
+# author          :Roberto Pinto
+# date            :2019.09.25
+# version         :1.0
+# notes           :This software is meant for teaching purpose only and it is provided as-is
+# python_version  :3.7
+# ==============================================================================
+
+
